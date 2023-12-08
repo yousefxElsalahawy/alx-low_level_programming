556a549093dcafc7ecb42daf522545bd29f1c032
root <yousef>
2023-12-08 06:30:57 -0800

    this is my new commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+	return (1);
+}
